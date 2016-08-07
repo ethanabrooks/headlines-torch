fa45f797cf1941b5b8b275c904f162d51f0a5ab7
Ethan <ethanabrooks@gmail.com>
2016-08-07 00:08:50 -0700

    rename decoder

--- decoder.lua
+++ decoder.lua
@@ -1,94 +1,112 @@
 require 'rnn'
 require 'nngraph'
 
+
 function buildDecoder(train, hiddenSize, inSeqLen, outSeqLen, weightedModules)
     -- weighted modules
-    local embedding, deepGRU, outLayer = unpack(weightedModules)
+    local embed, deepGRU, outLayer = unpack(weightedModules)
 
     --- build zipRepeating: {h, y} -> {{h, y1}, {h, y2}, ...}
-    local zipRepeating = nn.ConcatTable()
-    for i = 1, outSeqLen do
-        local parallel = nn.ParallelTable()
-                         :add(nn.Identity())   -- repeating
-                         :add(nn.Select(1, i)) -- nonrepeating
-        zipRepeating:add(parallel)
-    end
+    ---------------------------------------------------------
+    --- h:      [batchSize, inSeqLen, hiddenSize]
+    --- y:      [batchSize, outSeqLen, nClasses]
+    --- output: {{[batchSize, inSeqLen, hiddenSize], [batchSize, nClasses]}}
+    local y            = nn.SplitTable(2)()
+    local h            = nn.Identity()()
+    local output       = nn.ZipTableOneToMany(){h, y}
+    local zipRepeating = nn.gModule({h, y}, {output})
 
     --- from now on, y refers to yi and s refers to si
 
-    --- build start: {h, y} -> {h, y, s}
-    local h, y = nn.Identity()(), nn.Identity()()
-    local start = nn.gModule({ h, y }, { h, y, nn.Select(1, inSeqLen)(h) })
-                                            -- last state of encoder is
-                                            -- first state of decoder
-    --TODO: maybe not the best option
+    local input = nn.Identity()()          -- {y, h}
+    local h     = nn.SelectTable(1)(input) -- [batchSize, inSeqLen, hiddenSize]
+    local y     = nn.SelectTable(2)(input) -- [batchSize, nClasses]
+    local s     = nn.Identity()()          -- [batchSize, hiddenSize]
 
-    --- build merge for nn.Recurrent(): {{h, y}, {s, y_pred}} -> {h, y, s}
-    local merge
-    if train then
-        -- feed correct answer at each time step
-        local table, s   = nn.Identity()(), nn.SelectTable(1)()
-        local h          = nn.SelectTable(1)(table)
-        local y          = nn.SelectTable(2)(table)
-        merge            = nn.gModule({ table, s }, { h, y, s })
-    else
-        -- feed previous prediction back in
-        local h, table  = nn.SelectTable(1)(), nn.Identity()()
-        local s         = nn.SelectTable(1)(table)
-        local y_pred    = nn.SelectTable(2)(table)
-        local y         = nn.ArgMax(2, 2)(y_pred)
-        merge           = nn.gModule({ h, table }, { h, y, s })
-    end
+    local broadcast = nn.Replicate(inSeqLen, 2)(s)                 -- [batchSize, inSeqLen, hiddenSize]
+    local h2d       = nn.Reshape(-1, hiddenSize, false)(h)         -- [batchSize * inSeqLen, hiddenSize]
+    local s2d       = nn.Reshape(-1, hiddenSize, false)(broadcast) -- [batchSize * inSeqLen, hiddenSize]
 
-    --- build transfer: {h, y, s_tm1} -> {s, y_pred}
-    local make2d       = nn.Reshape(-1, hiddenSize, false) -- for weighting across timesteps and batches
-    local restoreShape = nn.Reshape(inSeqLen, -1, hiddenSize, false)
+    --- {s, h} -> attention over h
+    ------------------------------
+    --- s:      [inSeqLen * batchSize, hiddenSize]
+    --- h:      [inSeqLen * batchSize, hiddenSize]
+    --- output: [inSeqLen * batchSize, hiddenSize]
+    local align = nn.Sequential()
+    :add(nn.CosineDistance())              -- compare                   [inSeqLen * batchSize]
+    :add(nn.Reshape(-1, inSeqLen, false))  -- expose inSeqLen dimension [batchSize, inSeqLen]
+    :add(nn.SoftMax())                     -- softmax over inSeqLen dim [inSeqLen, batchSize]
+    :add(nn.Reshape(-1, false))            -- ravel                     [batchSize * inSeqLen]
+    :add(nn.Replicate(hiddenSize, 2))      -- broadcast                 [batchSize * inSeqLen, hiddenSize]
+    local attention = align{s2d, h2d}      --                           [batchSize * inSeqLen, hiddenSize]
 
-    local combine = nn.Sequential()        -- combines s_tm1 with embed(y)
-    combine:add(nn.CAddTable())            -- combine though addition
-    combine:add(nn.Replicate(inSeqLen, 1)) -- broadcast
-    combine:add(make2d)                    -- for alignment with h
-
-    local y, s_tm1 = embedding(), nn.Identity()()  -- both batchSize, hiddenSize
-    local h, y_s   = make2d(), combine{ y, s_tm1 } -- both inSeqLen * batchSize, hiddenSize
-    local align    = nn.Sequential()
-
-    align:add(nn.CosineDistance())         -- compare   (result: inSeqLen * batchSize)
-    align:add(nn.SoftMax())                -- normalize (result: inSeqLen * batchSize)
-    align:add(nn.Replicate(hiddenSize, 2)) -- broadcast (result: inSeqLen * batchSize, hiddenSize)
-    local attention = align{ h, y_s }
-
-    -- apply attention and pass through deep GRU
-    local process = nn.Sequential()
-
+    --- {attention, h} -> attention(h)
+    ----------------------------------
+    --- attention: [inSeqLen * batchSize, hiddenSize]
+    --- h:         [batchSize, hiddenSize]
+    --- output:    [batchSize, hiddenSize]
+    local apply = nn.Sequential()
     -- dot product
-    process:add(nn.CMulTable()) -- inSeqLen * batchSize, hiddenSize
-    process:add(restoreShape)   -- inSeqLen, batchSize, hiddenSize
-    process:add(nn.Sum(1))      -- batchSize, hiddenSize
+    :add(nn.CMulTable())                              -- [batchSize * inSeqLen, hiddenSize]
+    :add(nn.Reshape(-1, inSeqLen, hiddenSize, false)) -- [batchSize, inSeqLen, hiddenSize]
+    :add(nn.Sum(2))                                   -- [batchSize, hiddenSize]
+    local weighted_h = apply{attention, h2d}          -- [batchSize, hiddenSize]
 
---    process:add(deepGRU)                            -- batchSize, hiddenSize
-    process:add(nn.GRU(hiddenSize, hi))
-    local s = process{ h, attention }               -- batchSize, hiddenSize
-    local transfer = nn.gModule({ h, y, s_tm1 },    -- sizes specified above
-                                { s,                -- batchSize, hiddenSize,
-                                  outLayer(s) })    -- batchSize, nClasses }
+    --- {weighted_h, y, s} -> concat{weighted_h, y, s}
+    --------------------------------------------------
+    --- weighted_h: [batchSize, hiddenSize]
+    --- y:          [batchSize, hiddenSize]
+    --- s:          [batchSize, hiddenSize]
+    local concat = nn.Concat(2)
+    :add(nn.SelectTable(1))
+    :add(nn.SelectTable(2))
+    :add(nn.SelectTable(3))
+    local gruInput = concat{weighted_h, y, s} -- [batchSize, 2 * hiddenSize + nClasses]
+    local output   = deepGRU(gruInput)        -- [batchSize, hiddenSize]
+    local rm = nn.gModule({ input, s }, { output })
 
-    --- build module for nn.Sequencer
-    local module = nn.Sequential()
-    module:add(nn.Recurrent( -- Recurrent layer, the main workhorse of the decoder
-    --  { (on step 1, start ->) input, feedback } -> merge -> transfer
-        start,
-        nn.Identity(), -- input    {h, y} -> {h, y}
-        nn.Identity(), -- feedback {s, y_pred} -> {s, y_pred}
-        transfer,      --          {h, y, s_tm1} -> {s, y_pred}
-        outSeqLen,     -- rho
-        merge          --          {{h, y}, {s, y_pred}} -> {h, y, s}
-    ))
-    module:add(nn.SelectTable(2)) -- {s, y_pred} -> y_pred
-
+    local sequencer = nn.Sequencer(
+        nn.Sequential()
+        :add(nn.Recurrence(rm, hiddenSize, 2, 1))
+        :add(outLayer)
+    )
     local decoder = nn.Sequential()
-    decoder:add(zipRepeating)
-    decoder:add(nn.Sequencer(module, outSeqLen))
+    :add(zipRepeating)
+    :add(sequencer)
     return decoder
 end
+
+function test()
+    -- hyper-parameters
+    local batchSize = 2
+    local inSeqLen = 3
+    local outSeqLen = 4 -- sequence length
+    local hiddenSize = 5
+    local vocSize = 1000
+    local nClasses = outSeqLen * batchSize * 2
+    local depth = 3
+
+    local h = torch.range(1, inSeqLen * batchSize * hiddenSize)
+    :resize(batchSize, inSeqLen, hiddenSize)
+    local y = torch.range(1, outSeqLen * batchSize * nClasses)
+    :resize(batchSize, outSeqLen, nClasses)
+    print(y:size())
+    print(h:size())
+
+    local weighted = {nn.LookupTable(vocSize, hiddenSize), nn.GRU(hiddenSize * 2 + nClasses, hiddenSize), nn.Linear(hiddenSize, nClasses)}
+    local model = buildDecoder(false, hiddenSize, inSeqLen, outSeqLen, weighted)
+    local out = model:forward{ h, y }
+    print(out)
+    local criterion = nn.SequencerCriterion(nn.CrossEntropyCriterion())
+    local tgts = torch.ones(batchSize, outSeqLen):split(1, 2)
+    print(criterion:forward(out, tgts))
+--    local out = model:forward{ x, y2 }
+--    local gradOutput = model:backward({ x, y1 }, out)
+--    model.model:updateParameters(1)
+--    local out = model:forward{ x, y1 }
+--    local out = testModel:forward{ x, y2 }
+--    testModel:backward({ x, y2 }, out)
+end
+--test()
 return buildDecoder
+
---
+++ decoder.old.lua
@@ -0,0 +1,94 @@
+require 'rnn'
+require 'nngraph'
+
+function buildDecoder(train, hiddenSize, inSeqLen, outSeqLen, weightedModules)
+    -- weighted modules
+    local embedding, deepGRU, outLayer = unpack(weightedModules)
+
+    --- build zipRepeating: {h, y} -> {{h, y1}, {h, y2}, ...}
+    local zipRepeating = nn.ConcatTable()
+    for i = 1, outSeqLen do
+        local parallel = nn.ParallelTable()
+                         :add(nn.Identity())   -- repeating
+                         :add(nn.Select(1, i)) -- nonrepeating
+        zipRepeating:add(parallel)
+    end
+
+    --- from now on, y refers to yi and s refers to si
+
+    --- build start: {h, y} -> {h, y, s}
+    local h, y = nn.Identity()(), nn.Identity()()
+    local start = nn.gModule({ h, y }, { h, y, nn.Select(1, inSeqLen)(h) })
+                                            -- last state of encoder is
+                                            -- first state of decoder
+    --TODO: maybe not the best option
+
+    --- build merge for nn.Recurrent(): {{h, y}, {s, y_pred}} -> {h, y, s}
+    local merge
+    if train then
+        -- feed correct answer at each time step
+        local table, s   = nn.Identity()(), nn.SelectTable(1)()
+        local h          = nn.SelectTable(1)(table)
+        local y          = nn.SelectTable(2)(table)
+        merge            = nn.gModule({ table, s }, { h, y, s })
+    else
+        -- feed previous prediction back in
+        local h, table  = nn.SelectTable(1)(), nn.Identity()()
+        local s         = nn.SelectTable(1)(table)
+        local y_pred    = nn.SelectTable(2)(table)
+        local y         = nn.ArgMax(2, 2)(y_pred)
+        merge           = nn.gModule({ h, table }, { h, y, s })
+    end
+
+    --- build transfer: {h, y, s_tm1} -> {s, y_pred}
+    local make2d       = nn.Reshape(-1, hiddenSize, false) -- for weighting across timesteps and batches
+    local restoreShape = nn.Reshape(inSeqLen, -1, hiddenSize, false)
+
+    local combine = nn.Sequential()        -- combines s_tm1 with embed(y)
+    combine:add(nn.CAddTable())            -- combine though addition
+    combine:add(nn.Replicate(inSeqLen, 1)) -- broadcast
+    combine:add(make2d)                    -- for alignment with h
+
+    local y, s_tm1 = embedding(), nn.Identity()()  -- both batchSize, hiddenSize
+    local h, y_s   = make2d(), combine{ y, s_tm1 } -- both inSeqLen * batchSize, hiddenSize
+    local align    = nn.Sequential()
+
+    align:add(nn.CosineDistance())         -- compare   (result: inSeqLen * batchSize)
+    align:add(nn.SoftMax())                -- normalize (result: inSeqLen * batchSize)
+    align:add(nn.Replicate(hiddenSize, 2)) -- broadcast (result: inSeqLen * batchSize, hiddenSize)
+    local attention = align{ h, y_s }
+
+    -- apply attention and pass through deep GRU
+    local process = nn.Sequential()
+
+    -- dot product
+    process:add(nn.CMulTable()) -- inSeqLen * batchSize, hiddenSize
+    process:add(restoreShape)   -- inSeqLen, batchSize, hiddenSize
+    process:add(nn.Sum(1))      -- batchSize, hiddenSize
+
+--    process:add(deepGRU)                            -- batchSize, hiddenSize
+    process:add(nn.GRU(hiddenSize, hi))
+    local s = process{ h, attention }               -- batchSize, hiddenSize
+    local transfer = nn.gModule({ h, y, s_tm1 },    -- sizes specified above
+                                { s,                -- batchSize, hiddenSize,
+                                  outLayer(s) })    -- batchSize, nClasses }
+
+    --- build module for nn.Sequencer
+    local module = nn.Sequential()
+    module:add(nn.Recurrent( -- Recurrent layer, the main workhorse of the decoder
+    --  { (on step 1, start ->) input, feedback } -> merge -> transfer
+        start,
+        nn.Identity(), -- input    {h, y} -> {h, y}
+        nn.Identity(), -- feedback {s, y_pred} -> {s, y_pred}
+        transfer,      --          {h, y, s_tm1} -> {s, y_pred}
+        outSeqLen,     -- rho
+        merge          --          {{h, y}, {s, y_pred}} -> {h, y, s}
+    ))
+    module:add(nn.SelectTable(2)) -- {s, y_pred} -> y_pred
+
+    local decoder = nn.Sequential()
+    decoder:add(zipRepeating)
+    decoder:add(nn.Sequencer(module, outSeqLen))
+    return decoder
+end
+return buildDecoder
